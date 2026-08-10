d00114fe8569ea6a1821876f1372aa0ba3ab1f1e
Alhachmi01 <chtttiwiya@gmail.com>
2026-08-10 02:56:07 +0100

    Add contact page to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,7 +2,7 @@ import type { MetadataRoute } from "next";
 import { guides, siteConfig, tools } from "@/lib/site";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const staticRoutes = ["", "/tools", "/guides", "/about", "/privacy", "/terms"];
+  const staticRoutes = ["", "/tools", "/guides", "/about", "/contact", "/privacy", "/terms"];
 
   return [
     ...staticRoutes.map(route => ({ url: `${siteConfig.url}${route}`, lastModified: new Date() })),
